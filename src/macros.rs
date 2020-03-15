5e1200a8690499f508718d6544db66f690ff48e5
Norbytus <norbyt93@gmail.com>
2020-03-15 20:19:00 +0300

    Add macros for create function and module

--- src/macros.rs
+++ src/macros.rs
@@ -15,6 +15,7 @@ macro_rules! php_module {
         functions: [
             $([
                 $function_name:expr,
+                $handler:expr,
                 args: [
                     $([
                         $argument_name:expr,
@@ -24,24 +25,44 @@ macro_rules! php_module {
                     ]),* $(,)*
                 ]
               ]),* $(,)*
-        ] $(,)*
+        ]
     ) => {
-        use super::zend::ModuleBuilder;
+        use zend::ModuleBuilder;
 
         #[no_mangle]
         pub extern fn info() {
             print_table_start();
-            print_table_row(info);
+            print_table_row($module_info, "enabled");
             print_table_end();
         }
 
-        let mut module = ModuleBuilder::new(c_str!(name), c_str!(version))
-            .with_info_function(info);
 
-        functions
+        // $(php_function! {
+        //     name: $function_name,
+        //     args: [
+        //         $([$argument_name, $nullable, $variadic, $reference])*
+        //     ]
+        // })*
 
-        pub extern fn get_module() -> &mut zend::Module {
-            module
+        #[no_mangle]
+        pub extern fn get_module() -> *mut zend::Module {
+            let mut module = ModuleBuilder::new(
+                c_str!($module_name),
+                c_str!($module_version)
+            ).with_info_function(info);
+
+            $(
+                let f: zend::Function = php_function! {
+                    name: $function_name,
+                    handler: $handler,
+                    args: [
+                        $([$argument_name, $nullable, $variadic, $reference],)*
+                    ]
+                };
+                let module = module.with_function(f);
+            )*;
+
+            module.build().into_raw()
         }
     }
 }
@@ -50,6 +71,7 @@ macro_rules! php_module {
 macro_rules! php_function {
     (
         name: $function_name:expr,
+        handler: $handler:expr,
         args: [
             $([
                 $name:expr,
@@ -58,15 +80,48 @@ macro_rules! php_function {
                 $reference:expr
               ]),* $(,)*
         ] $(,)*
-    ) => {
+    ) => ({
+
+        let mut args: Vec<zend::ArgInfo> = Vec::new();
 
         $(
-            zend::ArgInfo::new(
+            args.push(zend::ArgInfo::new(
                 c_str!($name),
                 $nullable,
                 $variadic,
                 $reference
-            );
-        )*
-    }
+            ));
+        )*;
+
+        #[no_mangle]
+        extern "C" fn wrap_php_function(
+            data: &zend::ExecuteData,
+            retval: &mut zend::Zval
+        ) {
+            let mut first_arg = zend::Zval::new_as_null();
+            let mut second_arg = zend::Zval::new_as_null();
+
+            php_parse_parameters!(&mut first_arg, &mut second_arg);
+
+            let mut args: Vec<zend::Zval> = Vec::new();
+            args.push(first_arg);
+            args.push(second_arg);
+
+            let result = $handler(args);
+
+            php_return!(retval, result);
+        }
+
+        let mut function = zend::FunctionBuilder::new(
+            c_str!($function_name),
+            wrap_php_function
+        );
+
+        for arg in args {
+            function = function.with_arg(arg);
+        }
+
+
+        function.build()
+    })
 }
